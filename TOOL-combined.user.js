40cd76f0d23036256dbb48b092f0150d86726e29
Tân <35867834+pntan@users.noreply.github.com>
2025-04-04 13:23:50 +0700

    Update TOOL-combined.user.js

--- TOOL-combined.user.js
+++ TOOL-combined.user.js
@@ -7,15 +7,13 @@
 // @match        https://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=http://anonymouse.org/
 // @grant        GM_xmlhttpRequest
-// @require      https://code.jquery.com/jquery-3.7.1.min.js
-// @require      https://code.jquery.com/ui/1.14.1/jquery-ui.js
-// @require      https://cdn.jsdelivr.net/npm/dompurify@2.4.0/dist/purify.min.js
+// @updateURL    https://raw.githubusercontent.com/pntan/TPTOOL/main/TOOL-combined.user.js
+// @downloadURL  https://raw.githubusercontent.com/pntan/TPTOOL/main/TOOL-combined.user.js
+// @connect      raw.githubusercontent.com
 // ==/UserScript==
-/* globals       jQuery, $, waitForKeyElements */
 
 (function() {
     const rawScriptUrl = 'https://raw.githubusercontent.com/pntan/TPTOOL/main/TOOL.js?nocache=' + Date.now();
-    window.open(rawScriptUrl, "_blank");
 
     GM_xmlhttpRequest({
         method: 'GET',
@@ -24,8 +22,6 @@
             try {
                 eval(response.responseText);
                 console.log('[TOOL] Đã tải và chạy TOOL.js mới nhất từ GitHub.');
-                console.log(response);
-                console.log(response);
             } catch (e) {
                 console.error('[TOOL] Lỗi khi chạy script TOOL.js:', e);
             }
