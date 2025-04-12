ba5f0c320a09a4d6b68d2bace1551082c8456ad6
Tân <35867834+pntan@users.noreply.github.com>
2025-04-12 15:58:16 +0700

    Update TOOL-combined.user.js
    Thêm thư viện mới về excel

--- TOOL-combined.user.js
+++ TOOL-combined.user.js
@@ -1,16 +1,20 @@
 // ==UserScript==
-// @name         Công Cụ Hỗ Trợ GITHUB
+// @name         Công Cụ Hỗ Trợ
 // @namespace    http://tampermonkey.net/
 // @version      2025-01-02
 // @description  Bộ công cụ tích hợp các chức năng hỗ trợ cho sàn TMĐT
 // @author       TanPhan
 // @match        https://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=http://anonymouse.org/
-// @grant        GM_xmlhttpRequest
-// @updateURL    https://raw.githubusercontent.com/pntan/TPTOOL/main/TOOL-combined.user.js
-// @downloadURL  https://raw.githubusercontent.com/pntan/TPTOOL/main/TOOL-combined.user.js
-// @connect      raw.githubusercontent.com
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.7.1.min.js
+// @require      https://code.jquery.com/ui/1.14.1/jquery-ui.js
+// @require      https://cdn.jsdelivr.net/npm/dompurify@2.4.0/dist/purify.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/exceljs/4.3.0/exceljs.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js
 // ==/UserScript==
+/* globals       jQuery, $, waitForKeyElements */
 
 (function() {
     const rawScriptUrl = 'https://raw.githubusercontent.com/pntan/TPTOOL/main/TOOL.js?nocache=' + Date.now();
