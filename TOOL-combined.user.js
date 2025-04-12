d0d75d73a6c9167408ceca12954c66a2a8180d6e
Tân <35867834+pntan@users.noreply.github.com>
2025-04-12 16:13:43 +0700

    Update TOOL-combined.user.js

--- TOOL-combined.user.js
+++ TOOL-combined.user.js
@@ -1,18 +1,21 @@
 // ==UserScript==
-// @name         Công Cụ Hỗ Trợ
+// @name         Công Cụ Hỗ Trợ GITHUB
 // @namespace    http://tampermonkey.net/
 // @version      2025-01-02
 // @description  Bộ công cụ tích hợp các chức năng hỗ trợ cho sàn TMĐT
 // @author       TanPhan
 // @match        https://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=http://anonymouse.org/
-// @grant        none
+// @grant        GM_xmlhttpRequest
 // @require      https://code.jquery.com/jquery-3.7.1.min.js
 // @require      https://code.jquery.com/ui/1.14.1/jquery-ui.js
 // @require      https://cdn.jsdelivr.net/npm/dompurify@2.4.0/dist/purify.min.js
+// @require      https://code.jquery.com/jquery-3.7.1.min.js
+// @require      https://code.jquery.com/ui/1.14.1/jquery-ui.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/exceljs/4.3.0/exceljs.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js
+// @connect      raw.githubusercontent.com
 // ==/UserScript==
 /* globals       jQuery, $, waitForKeyElements */
 
