1525650e0b8850d1adae98112f4351738b74c8a0
Tân <35867834+pntan@users.noreply.github.com>
2025-04-19 12:01:42 +0700

    Update TOOL.js

--- TOOL.js
+++ TOOL.js
@@ -2,7 +2,7 @@
   'use strict';
   var createUI = false;
 
-    const VERSION = "1.1.4";
+    const VERSION = "1.1.4"; 
   /*var Jqu = document.createElement("script");
   Jqu.setAttribute("src", "https://code.jquery.com/jquery-3.7.1.min.js");
   Jqu.setAttribute("rel", "preload");
