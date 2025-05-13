75a8e7476aeb4589fd3799e5904ef5e229062fba
Tân <35867834+pntan@users.noreply.github.com>
2025-05-13 11:21:17 +0700

    Update TOOL.js

--- TOOL.js
+++ TOOL.js
@@ -1,29 +1,11 @@
-// ==UserScript==
-// @name         Công Cụ Hỗ Trợ Mới
-// @namespace    http://tampermonkey.net/
-// @version      2025-01-02
-// @description  Bộ công cụ tích hợp các chức năng hỗ trợ cho sàn TMĐT
-// @author       TanPhan
-// @match        https://*/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=http://anonymouse.org/
-// @grant        none
-// @require      https://code.jquery.com/jquery-3.7.1.min.js
-// @require      https://code.jquery.com/ui/1.14.1/jquery-ui.js
-// @require      https://cdn.jsdelivr.net/npm/dompurify@2.4.0/dist/purify.min.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/exceljs/4.3.0/exceljs.min.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js
-// ==/UserScript==
-/* globals       jQuery, $, waitForKeyElements */
 
-(function() {
 	'use strict';
 
 	// Trạng thái hiển thị của giao diện
 	var createUI = false;
 
 	// Phiên bản của chương trình
-	const VERSION = "2.0.0";
+	const VERSION = "2.1.1";
 
 	/*const Jqu = document.createElement("script");
 	Jqu.setAttribute("src", "https://code.jquery.com/jquery-3.7.1.min.js");
@@ -36,7 +18,6 @@
 	JquUI.setAttribute("rel", "preload");
 	document.head.appendChild(JquUI);*/
 
-	window.onload = function(){
 		// Danh sách thư viện cần thêm
 		const LIBRARIES = [
 			"https://code.jquery.com/jquery-3.7.1.min.js", // JQYERY
