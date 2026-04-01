1ddd9e566a2b76816e48e441fddd21d6eb0f4125
Tân <35867834+pntan@users.noreply.github.com>
2026-04-01 16:24:00 +0700

    Update TOOL.js

--- TOOL.js
+++ TOOL.js
@@ -4,7 +4,7 @@
   var createUI = false;
 
   // Phiên bản của chương trình
-  const VERSION = "2.12.6";
+  const VERSION = "2.12.7";
 
   /*var Jqu = document.createElement("script");
   Jqu.setAttribute("src", "https://code.jquery.com/jquery-3.7.1.min.js");
